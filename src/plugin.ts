4a3cecfba4b7c3d545ad6ca30650bbd46d0da7d3
Benny Powers <web@bennypowers.com>
2020-09-21 09:34:17 +0300

    fix: initialize without waiting on events

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,12 +1,8 @@
 import hljs from 'highlight.js';
 import { definer } from 'highlightjs-graphql';
-import { Application } from 'typedoc';
-import { Converter } from 'typedoc/dist/lib/converter';
 
 export class GraphQLPlugin {
-  initialize(app: Application): void {
-    app.on(Converter.EVENT_BEGIN, () => {
-      hljs.registerLanguage('graphql', definer);
-    });
+  initialize(): void {
+    hljs.registerLanguage('graphql', definer);
   }
 }
